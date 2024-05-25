1d9911a21bf03764a17174e870545623c650fbfe
yair pesoinboure <yairpesoinboure08@gmail.com>
2024-05-24 21:24:38 -0300

    agrego jquery y boton en password

--- login.js
+++ login.js
@@ -3,24 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(elems);
 });
 
-var clicked = 0;
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('select');
+   var instances = M.FormSelect.init(elems);
+});
 
-  $(".toggle-password").click(function (e) {
-     e.preventDefault();
-
-    $(this).toggleClass("toggle-password");
-      if (clicked == 0) {
-        $(this).html('<span class="material-icons">visibility_off</span >');
-         clicked = 1;
+$(document).ready(function() {
+  $(".toggle-password").click(function() {
+      var input = $($(this).attr("toggle"));
+      if (input.attr("type") === "password") {
+          input.attr("type", "text");
       } else {
-         $(this).html('<span class="material-icons">visibility</span >');
-          clicked = 0;
-       }
-
-    var input = $($(this).attr("toggle"));
-    if (input.attr("type") == "password") {
-       input.attr("type", "text");
-    } else {
-       input.attr("type", "password");
-    }
+          input.attr("type", "password");
+      }
+  });
 });
